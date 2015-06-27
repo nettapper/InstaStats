4ebc15f806c4b95645159f8412db7e37f378ab34
Conner D <connerdunn7399@gmail.com>
2015-06-27 12:05:07 -0600

    hello world works

--- src/instastats/InstaStats.java
+++ src/instastats/InstaStats.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package instastats;
+// package instastats;
 
 /**
  *
@@ -15,7 +15,8 @@ public class InstaStats {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello world");
         // TODO code application logic here
     }
-    
+
 }
