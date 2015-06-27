2cab8d2eb172d8815ede1424d30aed125369c079
Mohammad Al-Ahdal <mkaa00x@live.co.uk>
2015-06-27 16:22:09 -0600

    Checking to see if JSON works

--- InstaStats/src/instastats/InstaStats.java
+++ InstaStats/src/instastats/InstaStats.java
@@ -1,28 +1,42 @@
 /**
  * InstaStats.java
- * 
+ *
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * 
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
+ *
  * @author Calvin Bochulak, Conner Dunn, ahmedrashid
  */
 package instastats;
 
 import client.ClientControl;
+import org.json.*;
 
 public class InstaStats {
+    
+    public InstaStats() {
+        
+    }
 
-	public InstaStats() {
-		
-	}
-	
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ClientControl control = new ClientControl();
         String s = control.search("conner_d23", 2);
+        
+        JSONObject jo = new JSONObject(s);
+        JSONArray dataArray = jo.getJSONArray("data");
+        JSONObject person1 = dataArray.getJSONObject(0);
+        String id = person1.getString("id");
+        
+        System.out.println("HEY THERE");
+        
+        System.out.println(id);
+        
+        System.out.println("BYE THERE");
+        //Create Array of type String to include the Image IDs
+        //Loop through them all finding the 3 with the most likes
     }
-
+    
 }
