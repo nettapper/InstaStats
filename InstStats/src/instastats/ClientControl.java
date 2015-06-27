a2ef814b98f6e21a2842dfa3e62ee6056707a16f
Notbobbobby <notbobbobby@gmail.com>
2015-06-27 12:43:45 -0600

    Added ClientControl
    This allows for basic get/push commands

---
+++ InstStats/src/instastats/ClientControl.java
@@ -0,0 +1,96 @@
+/**
+ * ClientControl.java
+ * 
+ * An example client class that works with the GameCenter Server
+ * 
+ * @author Calvin Bochulak, Conner Dunn
+ */
+package instastats;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClientControl {
+		
+	public static final String IP_ADDRESS = "localhost";
+	public static final int PORT = 65535;
+	public static final String ADDRESS = "http://" + IP_ADDRESS + ":" + PORT;
+	
+	protected ArrayList<String> paths;
+	protected String userSessionID;
+	
+	public ClientControl() {
+		
+	}
+	
+	/**
+	 * Connects to server and sends the json string 'outputData'
+	 * 
+	 * @param path The path connecting to
+	 * @param gsonClientPack The json formated string to be sent to the server
+	 * 
+	 * @return String The data from the server
+	 */
+	public static String connect(String path, String gsonClientPack) {		
+		
+		System.out.println("Client attempting a connection to : " + ADDRESS + path); // Debugging
+		
+		HttpURLConnection connection;
+		URL link;
+		InputStream input;
+		String data = "";
+		DataOutputStream output;
+		
+		try {
+		    link = new URL(ADDRESS + path);
+		    connection = (HttpURLConnection) link.openConnection();
+		    connection.setAllowUserInteraction(true);
+		    connection.setRequestMethod("POST");
+		    connection.setDoOutput(true);
+		    connection.setDoInput(true);
+		    connection.setInstanceFollowRedirects(false);
+
+		    output = new DataOutputStream(connection.getOutputStream());
+		    output.writeBytes(gsonClientPack);
+		    output.flush();
+		    output.close();
+
+		    try {  //Try to read the data from server
+				input = connection.getInputStream();
+				while (input.available() <= 0); //no data from server, waiting...
+				while (input.available() > 0) {
+					int number = input.read();
+					data += (char) number;
+				}
+			    input.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				data = gsonClientPack; //in the case of an exception, return the original pack
+			}
+		    
+			connection.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// DEBUGGING //
+		
+		System.out.println("Client succesfully connected to   : " + ADDRESS + path); // Debugging
+		System.out.println("Data To Server: " + gsonClientPack);
+		System.out.println("Data From Server: " + data);
+		System.out.println("---------------------------------------");
+		
+		// END //
+		
+		return data;
+	}
+	
+	public static void main(String[] args) {
+		
+		new ClientControl();
+	}
+}
